fec525255930d09ef9a1e2cb9e8dfd1706d27523
prosperceed <osiletusuccess@gmail.com>
2025-08-19 03:30:21 +0000

    fixed prefix data

--- src/components/Home/Achievement/index.tsx
+++ src/components/Home/Achievement/index.tsx
@@ -19,15 +19,13 @@ const Achievement = () => {
 								className="flex flex-col items-center border border-cyan-400/70 gap-4 bg-background opacity-[0.5] py-4 md:py-8 px-5 md:px-6 rounded-md"
 							>
 								<h3 ref={ref} className="text-3xl font-black text-primary">
-									{item.prefix && item.prefix}
-									{item.count == 247 ? (
+									{item.count === 247 ? (
 										"24/7"
 									) : inView ? (
 										<CountUp start={0} end={item.count} duration={3} />
 									) : (
 										"0"
 									)}
-									{item.postfix && item.postfix}
 								</h3>
 								<p className="dark:text-white/80">{item.title}</p>
 							</div>
